e08a4e187f6f07573780803505b759e7618a2cb1
JonathanReb <john.reb97@gmail.com>
2018-04-14 04:50:24 -0500

    Funciones Niveles

---
+++ js/niveles.js
@@ -0,0 +1,24 @@
+function SelecNivel(id) {
+	switch(id){
+		case 1:
+			window.location.href = "nivelUno.html";
+			break;
+		case 2:
+			window.location.href = "nivelDos.html";
+			break;
+		case 3:
+			break;
+		case 4:
+			break;
+		case 5:
+			break;
+		case 6:
+			break;
+		case 7:
+			break;
+		case 8:
+			break;
+		case 9:
+			break;
+	}
+}
